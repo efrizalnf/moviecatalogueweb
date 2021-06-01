33efa2a4ff5ddc33389bc9bacbd1c4ed6e7161f7
efrizalnf <ondabuzz9@gmail.com>
2021-06-01 09:33:40 +0700

    added menu navigation menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -20,6 +20,13 @@ class App {
             content: this._content,
         });
     }
+
+     async renderPage() {
+         const url = UrlParser.parseActiveUrlWithCombiner();
+         const page = routes[url];
+         this._content.innerHTML = await page.render();
+         await page.afterRender();
+     }
 }
 
 export default App;
